22a9b5b5550eb0c1a3e9863c9a3ca4d31e15933a
Shubham Satpute <shubhamdsatpute77@gmail.com>
2026-01-04 01:14:07 +0530

    Docker setup for bot frontend and backend and docker compose file for one command build

--- backend/src/main/java/com/todo/todoapp/controller/TodoController.java
+++ backend/src/main/java/com/todo/todoapp/controller/TodoController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/todos")
 public class TodoController {
